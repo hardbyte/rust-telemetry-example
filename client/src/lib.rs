bba7d71ba6dd0c75ca2b5bc88e879ab954c7d810
Brian Thorne <brian@thorne.link>
2024-10-18 09:46:22 +1300

    Update lib.rs

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -8,11 +8,6 @@ pub use generated::*;
 /// State maintained by a [`Client`].
 /// Currently empty but required to use the with_pre_hook_async functionality
 /// with progenitor as of our pinned version https://github.com/oxidecomputer/progenitor/blob/4a3dfec3926f1f9db78eb6dc90087a1e2a1f9e45/progenitor-impl/src/method.rs#L1144-L1151
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct ClientState {}
 
-impl Default for ClientState {
-    fn default() -> Self {
-        ClientState {}
-    }
-}
